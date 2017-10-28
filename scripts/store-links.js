8f275409e60f7ba463b8b5943f345d3bdcd30c51
Grant Gumina <grantgumina@outlook.com>
2017-10-27 20:47:27 -0700

    initial commit

---
+++ scripts/store-links.js
@@ -0,0 +1,14 @@
+/*
+* Description:
+*   Store all links posted into a chat into a database
+*
+* Commands:
+*   None
+*
+*/
+
+module.exports = function(robot) {
+    robot.respond(/foobar/i, function(msg) {
+        msg.reply("FOOBAR");
+    });
+}
