749772b85978bcded8b322d9b0193d03fe4520af
Grant Gumina <grantgumina@outlook.com>
2017-10-27 21:11:40 -0700

    trying again

--- scripts/store-links.js
+++ scripts/store-links.js
@@ -16,4 +16,9 @@ function ScrapeLink(robot) {
     });
 }
 
-module.exports = ScrapeLink(robot);
+module.exports = function(robot) {
+    let urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i;
+    robot.respond(urlRegex, function(msg) {
+        msg.reply('I found a link: ' + msg.match[0]);
+    });
+}
