be8a8db1c3e62a8b410ddcaaf4d30434b3b60cd6
Grant Gumina <grantgumina@outlook.com>
2017-10-27 21:09:46 -0700

    trying out the regex function

--- scripts/store-links.js
+++ scripts/store-links.js
@@ -7,8 +7,13 @@
 *
 */
 
-module.exports = function(robot) {
-    robot.respond(/foobar/i, function(msg) {
-        msg.reply("FOOBAR");
+function ScrapeLink(robot) {
+
+    let urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i;
+
+    robot.hear(urlRegex, (msg) => {
+        msg.send('I found a link: ' + msg.match[0]);
     });
 }
+
+module.exports = ScrapeLink(robot);
