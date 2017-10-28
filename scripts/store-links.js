0c00d6cef826aab57ed0cb54dd66bd179c04a929
Grant Gumina <grantgumina@outlook.com>
2017-10-27 21:18:06 -0700

    does "let" not work??

--- scripts/store-links.js
+++ scripts/store-links.js
@@ -9,16 +9,23 @@
 
 function ScrapeLink(robot) {
 
-    let urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i;
+    var urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i;
 
-    robot.hear(urlRegex, (msg) => {
-        msg.send('I found a link: ' + msg.match[0]);
+    robot.hear(urlRegex, function(msg) {
+        // msg.send('I found a link: ' + msg.match[0]);
+        msg.send('I found a link');
     });
 }
 
-module.exports = function(robot) {
-    let urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i;
-    robot.respond(urlRegex, function(msg) {
-        msg.reply('I found a link: ' + msg.match[0]);
-    });
-}
+module.exports = ScrapeLink(robot);
+
+// module.exports = function(robot) {
+//     let urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i;
+//     robot.respond(urlRegex, function(msg) {
+//         msg.reply('I found a link: ' + msg.match[0]);
+//     });
+
+//     robot.hear(/foobar/i, function(msg) {
+
+//     })
+// }
