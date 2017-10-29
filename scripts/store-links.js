0bb091e6ef9bf9c636299265af07584007c4cd6c
Grant Gumina <grantgumina@outlook.com>
2017-10-29 15:52:49 -0700

    testing message id

--- scripts/store-links.js
+++ scripts/store-links.js
@@ -11,6 +11,8 @@ module.exports = function(robot) {
     var urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i;
     
     robot.hear(urlRegex, function(msg) {
-        msg.send('I found a link: ' + msg.match[0]);
+        // msg.send('I found a link: ' + msg.match[0]);
+        msg.send('msg id: ' + msg.message.id);
     });
 }
+
