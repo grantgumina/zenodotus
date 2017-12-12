bc4330749a84f313848152409ce5e459a3b30c90
Grant Gumina <grantgumina@outlook.com>
2017-12-11 20:45:19 -0800

    changing tag character to ^

--- scripts/Utilities/Constants.js
+++ scripts/Utilities/Constants.js
@@ -1,5 +1,5 @@
 const urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
-const tagRegex = /%([^\s][^0-9][a-z]*)/g;
+const tagRegex = /\^([^\s][^0-9][a-z]*)/g;
 
 class Constants {
     constructor() {
